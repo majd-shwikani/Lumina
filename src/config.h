93b43843ad06df37f1dd9d26fc9ea2af120941a5
majdshwikani <109483750+majd-shwikani@users.noreply.github.com>
2025-10-22 21:34:52 +0200

    Added A separate config file

---
+++ src/config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// WiFi credentials
+#define WIFI_SSID "Espressif"
+#define WIFI_PASSWORD "Centrino2121"
+
+// Firebase configuration
+#define FIREBASE_HOST "test-dbd7a-default-rtdb.firebaseio.com"
+#define FIREBASE_SECRET "jBVd9wZ6n1dYTv4CSVmB96l66M8HspblZSmCN161"
+
+// NeoPixel configuration
+#define LED_PIN 26
+#define NUM_LEDS 180
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,11 @@
 #include <Wire.h>
 #include <Adafruit_VEML7700.h>
 
+// Include configuration file
+#include "config.h"
+
 // Provide the token generation process info.
 #include <addons/TokenHelper.h>
-
-// WiFi credentials
-#define WIFI_SSID "Espressif"
-#define WIFI_PASSWORD "Centrino2121"
-
-// Firebase configuration
-#define FIREBASE_HOST "test-dbd7a-default-rtdb.firebaseio.com"
-#define FIREBASE_SECRET "jBVd9wZ6n1dYTv4CSVmB96l66M8HspblZSmCN161"
-
-// NeoPixel configuration
-#define LED_PIN 26
-#define NUM_LEDS 180
 
 // VEML7700 configuration
 volatile float luxThreshold = 1.0;  // Default value, will be controlled via Firebase
